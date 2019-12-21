ebf92b23e0b597f03723203eae73d9ef9d1da3e8
Lydia B <lydiabaik@gmail.com>
2019-12-21 23:56:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,6 +52,7 @@ holiday_hash.each do |seasons, supplies|
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+  
   # return an array of all of the supplies that are used in the winter season
 
 end
