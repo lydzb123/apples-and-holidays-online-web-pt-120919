df80af116f900c41343e7a0327c6186bebcf7240
Lydia B <lydiabaik@gmail.com>
2019-12-21 23:16:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_hash[:summer].values.each_with_index {|element, i| return element if i == 1 }
+  holiday_hash[:summer].values binding.pry
+  # .each_with_index {|element, i| return element if i == 1 }
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
