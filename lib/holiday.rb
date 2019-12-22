dcac75036a28cff509d25ea1c6a5b869fab58f4b
Lydia B <lydiabaik@gmail.com>
2019-12-22 02:16:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ def all_holidays_with_bbq(holiday_supplies)
   holiday_supplies.each do |season, holiday|
     holiday.each.collect do |name, supply|
       if supply.include?("BBQ")
-        holidays_with_bbqs << name
+       print name
       end
     end
   end
