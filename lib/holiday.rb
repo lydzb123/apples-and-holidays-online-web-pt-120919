ae8994c14502da0fdd7293cd4076e1089afb18f7
Lydia B <lydiabaik@gmail.com>
2019-12-22 00:31:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
     end
 end
 
-puts all_supplies_in_holidays({
+all_supplies_in_holidays({
   :winter => {
     :christmas => ["Lights", "Wreath"],
     :new_years => ["Party Hats"]
