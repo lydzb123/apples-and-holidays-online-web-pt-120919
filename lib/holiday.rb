4740c868234fb6824ebb9b2c15b79f4a10ec993d
Lydia B <lydiabaik@gmail.com>
2019-12-22 01:56:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,21 +71,6 @@ end
 
 
 
-holiday_hash = {
-  :winter => {
-    :christmas => ["Lights", "Wreath"],
-    :new_years => ["Party Hats"]
-  },
-  :summer => {
-    :fourth_of_july => ["Fireworks", "BBQ"]
-  },
-  :fall => {
-    :thanksgiving => ["Turkey"]
-  },
-  :spring => {
-    :memorial_day => ["BBQ"]
-  }
-}
 
 print all_supplies_in_holidays(holiday_hash)
 
