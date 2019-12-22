ed7466c7c58aa5d4b9f0acc4b864b6f72b564be7
Lydia B <lydiabaik@gmail.com>
2019-12-22 01:49:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -110,8 +110,8 @@ def all_holidays_with_bbq(holiday_hash)
       end
     end
   end
-  end
 end
+
 
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
